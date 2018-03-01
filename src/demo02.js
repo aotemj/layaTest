56f5f599d770341d4f28436058964b974b51c2fa
lingpj <ling2735880>
2018-03-01 13:53:36 +0800

    #demo03（文本换行，宽高设置）#

--- src/demo02.js
+++ src/demo02.js
@@ -1,29 +1,29 @@
 // 初始化舞台
-Laya.init(600,300);
-var txt = new Laya.Text();
+// Laya.init(600,300);
+// var txt = new Laya.Text();
 
-txt.text = "Hello LayaBox";
-// 设置字体大小
-txt.fontSize = 32;
+// txt.text = "Hello LayaBox";
+// // 设置字体大小
+// txt.fontSize = 32;
 
-// 设置字体颜色
-txt.color = "blue";
+// // 设置字体颜色
+// txt.color = "blue";
 
-// 设置字体加粗
-txt.bold = true;
+// // 设置字体加粗
+// txt.bold = true;
 
-// 设置字体倾斜
-txt.italic = true;
+// // 设置字体倾斜
+// txt.italic = true;
 
-// 设置字体背景色
-txt.bgColor = "red";
+// // 设置字体背景色
+// txt.bgColor = "red";
 
-// 设置边框颜色
-txt.borderColor = "yellow";
+// // 设置边框颜色
+// txt.borderColor = "yellow";
 
-// 设置字体位置
-txt.pos(60,100);
+// // 设置字体位置
+// txt.pos(60,100);
 
-Laya.stage.bgColor = "#000";
-Laya.stage.addChild(txt);
+// Laya.stage.bgColor = "#000";
+// Laya.stage.addChild(txt);
 
