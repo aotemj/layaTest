4d15b56eefe453cfab7486842ce7c1f42c2fd33a
lingpj <ling2735880>
2018-03-01 12:02:26 +0800

    #练习helloLayabox案例#

---
+++ src/demo01.js
@@ -0,0 +1,19 @@
+// 创建舞台，默认背景色是黑色的
+Laya.init(600,300);
+var txt = new Laya.text();
+
+// 设置文本颜色
+txt.color = "#f00";
+// 设置字体加粗：
+txt.bold = true;
+// 设置字体大小：
+txt.fontSize = 66;
+// 设置字体描边
+txt.stroke = 5;
+txt.strokeColor = "#fff";
+// 设置字体显示的起始位置
+txt.pos(60,100);
+// 设置舞台背景色
+Laya.stage.bgColor= "#232383";
+// 将文本添加到舞台
+Laya.stage.addChild(txt);
